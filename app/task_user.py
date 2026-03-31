19068244c42a2c31fdbaa6906895901d1da0f461
general-soldatov <root@DESKTOP-KVTP98R.localdomain>
2026-03-31 07:57:41 +0300

    Update data

--- app/task_user.py
+++ app/task_user.py
@@ -31,6 +31,17 @@ class TaskData:
         exec(txt_task, glbl, lcls)
         return lcls['text'], lcls['result']
 
+    @staticmethod
+    def derivative(f,a,method='central', h=0.01):
+        if method == 'central':
+            return (f(a + h) - f(a - h))/(2*h)
+        elif method == 'forward':
+            return (f(a + h) - f(a))/h
+        elif method == 'backward':
+            return (f(a) - f(a - h))/h
+        else:
+            print("Method must be 'central', 'forward' or 'backward'.")
+
     def get_list_tasks(self, group: str, level: str) -> List[Dict[str, str]]:
         return self.data[group][level]
 
